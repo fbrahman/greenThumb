491b1c0d9950ae39959a5547610a986954620150
Cindy Chen <cindyjan15@gmail.com>
2017-08-05 12:15:10 -0700

    added routes

--- routes/html-route.js
+++ routes/html-route.js
@@ -4,6 +4,8 @@ const express = require('express');
 
 const router = express.Router();
 
+const db = require("../models");
+
 //landing page
 router.get('/', (req, res, next) => {
     res.render('index');
@@ -14,6 +16,30 @@ router.get('/registration', (req, res, next) => {
     res.render('registration');
 });
 
+//login page
+router.get('/login', (req, res, next) => {
+    res.render('login');
+});
+
+//dashboard
+router.get('/dashboard', (req, res, next) => {
+    res.render('dashboard');
+});
+
+//search
+router.get('/search',(req, res, next) => {
+    let plantName = req.body.plantName;
+    db.plants.findAll({
+        where: {
+            name: plantName
+        }
+    }).then(function(data) {
+        let hbsObject = {
+            results: data
+        }
+    res.render('results', hbsObject);
+    });
+});
 
 module.exports = router;
 
